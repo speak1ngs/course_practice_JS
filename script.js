ca5825301c10ae5d31b7f642ba43a9da33562090
speak1ngs <juanmavillalbaovelar@gmail.com>
2023-03-02 06:29:46 -0300

    Agregar gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('parrafitoID');
+const input = document.querySelector('input');
+console.log
+({
+	h1,
+	p,
+	parrafito,
+	pid,
+	input}
+);
+
+h1.innerHTML= 'Patito';
